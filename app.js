bce42d9a367d0664477bc0b27ac0e65f2f508a5c
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-22 15:23:35 -0700

    map added to Lending page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var carListRouter = require('./routes/carList');
+var lendingRouter = require('./routes/Lending');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/carList', carListRouter);
+app.use('/Lending', lendingRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
