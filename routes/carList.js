ded528a9b2d8c2df72fa25756b5bc5a80c834b59
Phillip Chin <ekkus93@gmail.com>
2018-09-22 18:47:57 -0700

    fixed json with credentials

--- routes/carList.js
+++ routes/carList.js
@@ -5,6 +5,8 @@ var smartcar = require("../lib/smartcar");
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var credentials = smartcar.get_credentials();
+    console.log("credentials: " + credentials);    
+    console.log("credentials['accessToken']: " + credentials['accessToken']);
     smartcar.get_vehicles(credentials.accessToken, function(res) {
         console.log(JSON.stringify(res));
         res.render('carList', res);
