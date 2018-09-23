66d353e93bd9e6b823d465add23be371737aa129
Phillip Chin <ekkus93@gmail.com>
2018-09-23 05:03:21 -0700

    fixed lending page

--- routes/lending.js
+++ routes/lending.js
@@ -5,7 +5,7 @@ const models = require('../models/user.model');
 const smartcar = require("../lib/smartcar");
 const google_maps = require("../lib/google_maps");
 
-const GOOGLE_API_KEY = "<<fill in later>>";
+const GOOGLE_API_KEY = "AIzaSyDvWHFx3eLPTVVvsfEn-iFxt46jwKFiJYM";
 
 router.get('/', function(req, res, next) {
     console.log("###cookie user name: " + req.cookies.user);
@@ -15,20 +15,23 @@ router.get('/', function(req, res, next) {
                 res.render('lendingDone', { car: car});
             } else {
                 console.log("###lending");
-                var vehicle = smartcar.get_vehicle(userFromDb.credentials.accessToken, car.smartcar_id);
-                smartcar.get_vehicle_location(vehicle, function(loc_res) {
-                    console.log("###loc_res: " + JSON.stringify(loc_res));
-                    var data = {
-                        car: car,
-                        google_api_key: GOOGLE_API_KEY,
-                        lat: loc_res.data.latitude,
-                        lng: loc_res.data.longitude
-                    };
+                models.get_user_by_name(req.cookies.user, function(err, docs) {
+                    var userFromDb = docs[0];
+                    var vehicle = smartcar.get_vehicle(userFromDb.credentials.accessToken, car.smartcar_id);
+                    smartcar.get_vehicle_location(vehicle, function(loc_res) {
+                        console.log("###loc_res: " + JSON.stringify(loc_res));
+                        var data = {
+                            car: car,
+                            google_api_key: GOOGLE_API_KEY,
+                            lat: loc_res.data.latitude,
+                            lng: loc_res.data.longitude
+                        };
 
-                    google_maps.get_street_address(GOOGLE_API_KEY, data.lat, data.lng, function(street_address) {
-                        data['street_address'] = street_address;
+                        google_maps.get_street_address(GOOGLE_API_KEY, data.lat, data.lng, function(street_address) {
+                            data['street_address'] = street_address;
 
-                        res.render('lendingPrepare', data);
+                            res.render('lendingPrepare', data);
+                        });
                     });
                 });
             }
