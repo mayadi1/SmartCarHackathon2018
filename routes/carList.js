7b998de3a7c753108df96a2c13e95dac9b08bda4
Phillip Chin <ekkus93@gmail.com>
2018-09-22 23:02:00 -0700

    pulled car data from api for carList

--- routes/carList.js
+++ routes/carList.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
         smartcar.get_vehicles(credentials.accessToken, function(vehicles) {
             console.log("before router_res: " + JSON.stringify(vehicles));
 
-            router_res.render('carList', {data: vehicles});
+            router_res.render('carList', { "vehicles": vehicles});
             console.log("###after render");
         });
     };
